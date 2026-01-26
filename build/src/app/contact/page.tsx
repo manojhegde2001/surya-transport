51ef255623a2664a4c54cb2dd365fe4b0747cd08
Manoj Hegde <manoj.h@invenzolabs.com>
2026-01-27 00:11:19 +0530

    test

--- build/src/app/contact/page.tsx
+++ build/src/app/contact/page.tsx
@@ -14,7 +14,7 @@ const clientNames = [
 
 export default function ContactPage() {
   const [submitting, setSubmitting] = useState(false);
-
+ 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSubmitting(true);
